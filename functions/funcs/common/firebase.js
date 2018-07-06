50a55f135b2bc2cda6769bd52d1eb988dc45a5bd
Seita Uchimura <c201552k@gmail.com>
2018-07-07 00:02:59 +0900

    :watermelon: Add:firebase configにunicon-ff543のkeyを追加

---
+++ functions/funcs/common/firebase.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+let serviceAccount;
+if (process.env.NODE_ENV === 'test') {
+  serviceAccount = require('./keys/unicon-test-firebase-adminsdk.json');
+} else {
+  serviceAccount = functions.config().unicon.cert;
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
+});
+
+const db = admin.firestore();
+
+const firebase = {
+  functions,
+  admin,
+  db
+};
+
+module.exports = firebase;
